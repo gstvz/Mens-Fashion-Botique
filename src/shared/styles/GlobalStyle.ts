4091dcfb5bdb1dd063129c0444578adb12806e88
Gustavo França <gstvz.franca@gmail.com>
2022-04-21 13:44:38 -0300

    refactor: adjusted GlobalStyle's CSS Reset

--- src/shared/styles/GlobalStyle.ts
+++ src/shared/styles/GlobalStyle.ts
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    outline: 0;
+    outline: none;
     box-sizing: border-box;
   };
 
@@ -14,7 +14,8 @@ export const GlobalStyle = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
-    min-height: 100vh;
+    height: 100%;
+    overflow: hidden;
   };
 
   a {
@@ -23,7 +24,12 @@ export const GlobalStyle = createGlobalStyle`
   };
 
   button {
+    border: none;
     cursor: pointer;
+  };
+
+  input {
+    border: none;
   };
   
   li {
